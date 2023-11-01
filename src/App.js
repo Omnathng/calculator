6ce35713c6c58638d958026a65527fa4ea62d6ba
Omnadhng <omnadhkannan@gmail.com>
2023-11-02 00:04:48 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,12 @@
-import logo from './logo.svg';
+
 import './App.css';
+import Calc from './components/Calc'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1 style={{textAlign:'center',position:'relative'}}>Simple Calculator</h1>
+      <Calc/>
     </div>
   );
 }
